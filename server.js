d875b8489da5292294d7dc3066e95593aa3c60da
soulxmark <mark2899estimada@outlook.ph>
2024-04-27 23:27:46 +0800

    "Moved header include to head partial, added navigation partial to layout, and updated server.js to use express-ejs-layouts and specify layout file path"

--- server.js
+++ server.js
@@ -1,44 +1,43 @@
 /* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
+* This server.js file is the primary file of the 
+* application. It is used to control the project.
+*******************************************/
 /* ***********************
- * Require Statements
- *************************/
+* Require Statements
+*************************/
 const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+ 
 /* ***********************
- * Routes
- *************************/
+* View Engine and Templates
+*************************/
+app.set("view engine", "ejs");
+app.set("layout", "./layouts/layout"); // Specify the layout file path
+app.use(expressLayouts);
+ 
+/* ***********************
+* Routes
+*************************/
 app.use(static)
 //Index route
 app.get("/", function(req, res) {
   res.render("index", { title: "Home" });
 });
-
+ 
 /* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs");
-app.set("layout", "./layouts/layout"); // Specify the layout file path
-app.use(expressLayouts);
-
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+* Local Server Information
+* Values from .env (environment) file
+*************************/
 const port = process.env.PORT
 const host = process.env.HOST
-
+ 
 /* ***********************
- * Log statement to confirm server operation
- *************************/
+* Log statement to confirm server operation
+*************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
-
-
+ 

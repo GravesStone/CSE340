3e048d8fd019d32c6340fa96a59a13c6650d730f
soulxmark <mark2899estimada@outlook.ph>
2024-05-10 00:51:41 +0800

    chnages

--- server.js
+++ server.js
@@ -1,56 +1,74 @@
 /* ******************************************
-* This server.js file is the primary file of the 
-* application. It is used to control the project.
-*******************************************/
+ * This server.js file is the primary file of the
+ * application. It is used to control the project.
+ *******************************************/
+
+const pool = require("../database/");
 
 /* ***********************
-* Require Statements
-*************************/
-const expressLayouts = require("express-ejs-layouts")
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const utilities = require("./utilities")
-const baseController = require("./controllers/baseController")
+ * Require Statements
+ *************************/
+const expressLayouts = require("express-ejs-layouts");
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const utilities = require("./utilities/index");
+const baseController = require("./controllers/baseController");
 /* ***********************
-* View Engine and Templates
-*************************/
+ * View Engine and Templates
+ *************************/
 app.set("view engine", "ejs");
 app.set("layout", "./layouts/layout"); // Specify the layout file path
 app.use(expressLayouts);
- 
 /* ***********************
-* Routes
-*************************/
-app.use(static)
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+/* ***********************
+ * Routes
+ *************************/
+app.use(static);
 //Index route
-app.get("/", utilities.handleErrors(baseController.buildHome))
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
- /* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
   res.render("errors/error", {
-    title: err.status || 'Server Error',
+    title: err.status || "Server Error",
     message,
-    nav
-  })
-})
+    nav,
+  });
+});
 /* ***********************
-* Local Server Information
-* Values from .env (environment) file
-*************************/
-const port = process.env.PORT
-const host = process.env.HOST
- 
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT;
+const host = process.env.HOST;
+
 /* ***********************
-* Log statement to confirm server operation
-*************************/
+ * Log statement to confirm server operation
+ *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});

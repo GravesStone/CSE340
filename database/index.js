1bf82e5d4effeaa47a884c59ca27fdf2389e83d8
soulxmark <mark2899estimada@outlook.ph>
2024-05-14 22:48:05 +0800

    chnages

--- database/index.js
+++ database/index.js
@@ -1,37 +1,40 @@
-const { Pool } = require("pg")
-require("dotenv").config()
+const { Pool } = require("pg");
+require("dotenv").config();
+
 /* ***************
  * Connection Pool
  * SSL Object needed for local testing of app
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool
+
+let pool;
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-})
-//
-// Added for troubleshooting queries
-// during development
-module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
-    } catch (error) {
-      console.error("error in query", { text })
-      throw error
-    }
-  },
-}
+  });
+
+  // Export a custom query method for logging during development
+  module.exports = {
+    async query(text, params) {
+      try {
+        const res = await pool.query(text, params);
+        console.log("executed query", { text });
+        return res;
+      } catch (error) {
+        console.error("error in query", { text });
+        throw error;
+      }
+    },
+  };
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL_INTERNAL, // Changed to Internal
-  })
-  module.exports = pool
+  });
+
+  // Export the pool instance directly for production
+  module.exports = pool;
 }
